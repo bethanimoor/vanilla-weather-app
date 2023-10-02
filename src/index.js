82d53a84e143f8eba231a0cdcc1e54b98faf28dd
bethanimoor <bethanimoor@gmail.com>
2023-10-02 21:01:48 +0100

    Updated weather icon

--- src/index.js
+++ src/index.js
@@ -29,12 +29,19 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windspeedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let city = "New York";
@@ -42,5 +49,4 @@ let units = "metric";
 let apiKey = "f56bc44273a234408f2ba479f2f74294";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
